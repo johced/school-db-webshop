5fa9695173e010e272bad186e13644cc68826269
marv-ink <marvin96k@gmail.com>
2021-03-11 16:13:18 +0100

    all team members-Created the first user model

---
+++ model/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 40,
+  },
+  cryptoUrl: {
+    type: String,
+  },
+  cryptoUrlExpiration: {
+    type: Date,
+  },
+  role: {
+    type: String,
+  },
+});
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
