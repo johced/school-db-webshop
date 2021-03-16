ae647430bb4b785c6e60560dfa6fb1625be69a23
johced <johan.cederstrand@bahnhof.se>
2021-03-16 12:06:56 +0100

    All team members: We decided to always show wishlist icon in menu.

--- server/controller/wishlistController.js
+++ server/controller/wishlistController.js
@@ -1,31 +1,31 @@
-const User = require("../model/user");
+const User = require('../model/user');
 
-//Show Wishlist
+// show wish list
+exports.wishListShow_get = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.user.userDB._id }).populate('wishList');
+    // user.addWish(user);
+    res.render('wishlist.ejs', {
+      wishes: user.wishList,
+      user: req.user.userDB,
+    });
+    // console.log(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-exports.wishListShow_get = async(req, res) => {
+exports.wishList_get = async (req, res) => {
+  const productId = req.params.id;
 
-    try {
-        const user = await User.findOne({ _id: req.user.userDB._id }).populate("wishList");
-        res.render('wishlist.ejs', {
-            wishes: user.wishList,
-            user: req.user.userDB,
-        });
+  try {
+    const user = await User.findOne({ _id: req.user.userDB._id });
+    //  console.log(user);
+    user.addWish(productId, user);
 
-
-    } catch (err) {
-        console.log(err);
-    }
-
-}
-
-exports.wishList_get = async(req, res) => {
-    const productId = req.params.id;
-
-    try {
-        const user = await User.findOne({ _id: req.user.userDB._id });
-        user.addWish(productId, user);
-        res.redirect('back');
-    } catch (err) {
-        console.log(err);
-    }
-}
+    res.redirect('back');
+    // console.log(user.wishList);
+  } catch (err) {
+    console.log(err);
+  }
+};
