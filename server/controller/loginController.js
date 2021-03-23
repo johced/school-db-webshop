261ec65ca8340a1653202d735f8d124ab57c331b
LouLapins <loulapins@hotmail.com>
2021-03-23 09:52:14 +0100

    All team members: Minor styling fixes

--- server/controller/loginController.js
+++ server/controller/loginController.js
@@ -5,42 +5,42 @@ require('dotenv').config();
 
 // Login
 exports.login_get = (req, res) => {
-  if (req.cookies.jwtToken) return res.redirect('/userStart');
-  try {
-    res.render('login.ejs', { message: '' });
-  } catch (err) {
-    console.log(err);
-  }
+    if (req.cookies.jwtToken) return res.redirect('/userStart');
+    try {
+        res.render('login.ejs', { message: '' });
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
+exports.login_post = async(req, res) => {
+    const { email, password } = req.body;
 
-  try {
-    const userDB = await User.findOne({ email: email });
-    if (!userDB)
-      return res.render('login.ejs', {
-        message: 'Usr name or pwd do not exist',
-      });
+    try {
+        const userDB = await User.findOne({ email: email });
+        if (!userDB)
+            return res.render('login.ejs', {
+                message: 'Wrong user name or password',
+            });
 
-    const validUser = await bcrypt.compare(password, userDB.password);
+        const validUser = await bcrypt.compare(password, userDB.password);
 
-    if (!validUser)
-      return res.render('login.ejs', {
-        message: 'Usr name or pwd do not exist',
-      });
+        if (!validUser)
+            return res.render('login.ejs', {
+                message: 'Wrong user name or password',
+            });
 
-    const jwtToken = await jwt.sign({ userDB: userDB }, process.env.SECRET_KEY);
+        const jwtToken = await jwt.sign({ userDB: userDB }, process.env.SECRET_KEY);
 
-    if (jwtToken) {
-      const cookie = req.cookies.jwtToken;
-      if (!cookie) {
-        res.cookie('jwtToken', jwtToken, { maxAge: 10000000, httpOnly: true });
-      }
-      req.flash('success_msg', 'You are now logged in!');
-      res.redirect('/userStart');
+        if (jwtToken) {
+            const cookie = req.cookies.jwtToken;
+            if (!cookie) {
+                res.cookie('jwtToken', jwtToken, { maxAge: 10000000, httpOnly: true });
+            }
+            req.flash('success_msg', 'You are now logged in!');
+            res.redirect('/userStart');
+        }
+    } catch (err) {
+        console.log(err);
     }
-  } catch (err) {
-    console.log(err);
-  }
 };
